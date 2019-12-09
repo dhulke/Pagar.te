ead6089f2db8525c439afab380b6ca643ede3aec
Danilo S. Moraes <moraesdno@gmail.com>
2019-12-09 13:29:33 -0300

    add different npm test scripts for memory and postgres

--- src/usecase/transaction/PostgresTransactionRepository.js
+++ src/usecase/transaction/PostgresTransactionRepository.js
@@ -10,43 +10,30 @@ class PostgresTransactionRepository {
     }
 
     async findTransactionsByUserId(userId) {
-        try {
-            const transactions = [];
-            const transactionsDto = await this.knex("transaction").where({userId: userId});
-            for(const transactionDto of transactionsDto) {// single select is better.
-                const payables = await this.knex("payable").where({transactionId: transactionDto.id})
-                transactionDto.payables = payables;
-                const transaction = new Transaction(transactionDto);
-                transactions.push(transaction);
-            }
-            return transactions;
-        } catch(error) {
+        const transactions = [];
+        const transactionsDto = await this.knex("transaction").where({userId: userId});
+        for(const transactionDto of transactionsDto) {// single select is better.
+            const payables = await this.knex("payable").where({transactionId: transactionDto.id})
+            transactionDto.payables = payables;
+            const transaction = new Transaction(transactionDto);
+            transactions.push(transaction);
         }
-
+        return transactions;
     }
 
     async add(transactionEntity) {
-        try {
-            const transactionMapper = new TransactionMapper;
-            const dto = transactionMapper.toPersistenceDto(transactionEntity);
-            const payables = dto.payables;
-            delete dto.payables;
+        const transactionMapper = new TransactionMapper;
+        const dto = transactionMapper.toPersistenceDto(transactionEntity);
+        const payables = dto.payables;
+        delete dto.payables;
 
-            const transactionId = await this.knex("transaction").insert(dto).returning("id");
-            for(const payable of payables) {
-                payable.transactionId = parseInt(transactionId);
-            }
-            await this.knex("payable").insert(payables);
-        } catch(error) {
-            console.log(error)
+        const transactionId = await this.knex("transaction").insert(dto).returning("id");
+        for(const payable of payables) {
+            payable.transactionId = parseInt(transactionId);
         }
-
+        await this.knex("payable").insert(payables);
     }
 
-    static async clearDatabase() {
-        await this.knex("transaction").del();
-        await this.knex("payable").del();
-    }
 }
 
 module.exports = { PostgresTransactionRepository };
